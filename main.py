e8b80d0d0c0d2303d33f2402ac3c5e7f931c1767
Abdulrahman Itman <abdoitman@gmail.com>
2025-08-23 04:05:51 +0300

    Placeholders for the main script

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 ### Contain the entry point for the game
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import faiss
+from game import CodenameGame, Spymaster, FieldOperative
+
+if __name__ == '__main__':
+    LMmodel = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    index = faiss.read_index("word_embeddings.index")
+    id_to_word = np.load("id_to_word.npy", allow_pickle=True).item()
+    
+    game = CodenameGame()
+    key_card = game.key_card
+    cards = game.get_word_list()
+
+    blue_SM = Spymaster(key_card= key_card,
+                        team = 'blue',
+                        model= LMmodel,
+                        index= index)
+    blue_FO = FieldOperative(cards= cards,
+                             team = 'blue',
+                             model= LMmodel,
+                             index= index)
+    red_SM = Spymaster(key_card= key_card,
+                        team = 'red',
+                        model= LMmodel,
+                        index= index)
+    red_FO = FieldOperative(cards= cards,
+                             team = 'red',
+                             model= LMmodel,
+                             index= index)
+    
+    game.play(
+        blue_team= (blue_SM, blue_FO),
+        red_team= (red_SM, red_FO),
+        render= True
+    )
